11abc4fa1fad24c4da55d3b79e7756f6a6d11a40
Z-Den <71875639+Z-Den@users.noreply.github.com>
2021-03-14 15:44:13 +0500

    Add files via upload

---
+++ testing/testing.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+	fstream file("chisla.txt");
+
+	double a[500], max = 0;
+	int i = 0;
+
+	while (file && i < 500) {
+		file >> a[i];
+		i++;
+	}
+	double s = a[0];
+	for (int j = 1; j < 500; j++) {
+		if (a[j - 1] < a[j]) {
+			s += a[j];
+		}
+		else{
+			if (s > max) {
+				max = s;
+			}
+			s = a[j];
+		}
+	}
+
+	cout << max;
+	
+	return 0;
+}
